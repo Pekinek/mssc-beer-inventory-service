37a64df93849ac3726356a053aa9b6b7f875e071
Michał Mocek <pekinhere@gmail.com>
2021-11-21 19:10:25 +0100

    add allocation service

---
+++ src/main/java/guru/sfg/beer/inventory/service/services/AllocationService.java
@@ -0,0 +1,8 @@
+package guru.sfg.beer.inventory.service.services;
+
+import com.mmocek.commons.model.BeerOrderDto;
+
+public interface AllocationService {
+
+    Boolean allocateOrder(BeerOrderDto beerOrderDto);
+}
